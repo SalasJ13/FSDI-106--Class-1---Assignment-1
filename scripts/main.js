0a1670d1fa03e41d040163eeb7078df89c98604e
Salas <94945634+SalasJ13@users.noreply.github.com>
2022-01-25 17:00:26 -0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,61 @@
+//clase 2
+var isImportant = false;
+var isAsideVisible = true;
 
+function toggleImportant(){
+    console.log("Icon click")
+
+    let icon=$(".iImportant");
+    if(isImportant){
+        icon.removeClass("fas").addClass("far");
+        isImportant = false;
+    }
+    else{
+        icon.removeClass("far").addClass("fas");
+        isImportant =   true;
+    }
+    
+}
+function toggleDetails(){
+    let aside =$("aside");
+
+    if(isAsideVisible){
+        aside.hide();
+        isAsideVisible=false;
+    }
+    else{
+        aside.show();
+        isAsideVisible=true;
+    }
+}
+
+
+function saveTask(){
+    console.log("Saving task");
+
+    //guardamos lo que se ingresa
+    let text = $("#txtTitle").val();
+    console.log(text);
+    //tarea 2 mostrar todos lo que se ingrese desde el form y mostrarlo en console
+    let dateTime = $("#txtDueDate").val();
+    console.log(dateTime);
+    let location=$("#txtLocation").val();
+    console.log(location);
+    let description=$("#txtDescri").val();
+    console.log(description);
+    let participants=$("#txtParti").val();
+    console.log(participants);
+    let color =$("#txtColor").val();
+    console.log(color);
+}
+
+function init(){
+    console.log("Calendar")
+
+    //hook events
+    $("#btnSave").click(saveTask);
+
+    $(".iImportant").click(toggleImportant);
+    $("#btnToggleDet").click(toggleDetails);
+}
+window.onload=init;
